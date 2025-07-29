f303df6a2dec017c0e6892398da6b07be96bdb29
4uhar1k <vovas0712@gmail.com>
2025-07-29 15:25:44 +0200

    Updated Entities "Kaboo" and "Player"

--- src/main/kotlin/entity/Kaboo.kt
+++ src/main/kotlin/entity/Kaboo.kt
@@ -1,3 +1,8 @@
 package entity
 
-class Kaboo() {}
+class Kaboo {
+    val currentPlayer: Player? = null
+    val players = mutableListOf<Player>()
+    val drawNewStack = mutableListOf<Card>()
+    val drawUsedStack = mutableListOf<Card>()
+}
--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -1,3 +1,8 @@
 package entity
 
-data class Player(val name: String? = "Player", val knocked: Boolean, val viewedCards: Boolean) {}
+data class Player(val name: String? = "Player", val knocked: Boolean, val viewedCards: Boolean) {
+    val hand : Card? = null
+    val ownSelected : Card? = null
+    val otherSelected : Card? = null
+    val deck = mutableListOf<Card>()
+}
