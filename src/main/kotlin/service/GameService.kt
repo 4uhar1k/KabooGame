71bef0470ae7e38da9b0601e91928bdcf00c3ab5
4uhar1k <vovas0712@gmail.com>
2025-07-31 10:16:20 +0200

    Second version of "GameVersion" completed

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -2,33 +2,106 @@ package service
 import entity.*
 import tools.aqua.bgw.util.Stack
 import java.util.Collections.addAll
+import kotlin.IllegalStateException
 
 class GameService (private val rootService: RootService){
+    val kaboo = Kaboo()
     var player1 = Player()
     var player2 = Player()
-    fun StartGame(){
-        var kaboo = Kaboo()
+    fun startGame(){
         kaboo.currentPlayer = player1
         kaboo.players.add(player1)
         kaboo.players.add(player2)
-        kaboo.newStack = randomizeCards()
+        kaboo.newStack = createDeck()
+        giveStartCards()
         rootService.currentGame = kaboo
     }
     fun addPlayers(namePlayer1: String, namePlayer2: String){
         player1 = Player(namePlayer1)
         player2 = Player(namePlayer2)
     }
-
-    private fun randomizeCards() : Stack<Card> {
-        var randomListOfCards = List(52){ index ->
+    fun createDeck() : Stack<Card> {
+        val randomListOfCards = List(52){ index ->
             Card(
-                CardSuit.values()[index / 13],
-                CardValue.values()[index % 13]
+                CardSuit.entries[index / 13],
+                CardValue.entries[index % 13]
             )
         }.shuffled().toMutableList()
-        player1.deck = randomListOfCards.subList(0,4)
-        player1.deck = randomListOfCards.subList(4,8)
-        randomListOfCards.removeAll(randomListOfCards.subList(0,8))
         return Stack<Card>().apply { addAll(randomListOfCards) }
+    }
+    fun giveStartCards(){
+        for (i in 0..3){
+            player1.deck.add(kaboo.newStack.peek())
+            kaboo.newStack.pop()
+        }
+        for (i in 0..3){
+            player2.deck.add(kaboo.newStack.peek())
+            kaboo.newStack.pop()
+        }
+    }
+    fun gameMove(){
+        val kaboo = rootService.currentGame
+        if (kaboo == null){
+            throw IllegalStateException("Game not started yet")
+        }
+
+
+        var otherPlayer = Player()
+        if (kaboo.currentPlayer == player1)
+            otherPlayer = player2
+        else
+            otherPlayer = player1
+
+
+        if (kaboo.currentPlayer?.knocked == true){
+            endGame()
+        }
+        if (otherPlayer.knocked == true){
+            //knocked not allowed anymore
+        }
+        val drawFromNewStack = false // should be updated by actions in gui
+
+        if (drawFromNewStack){
+            kaboo.currentPlayer?.hand = kaboo.newStack.peek()
+            kaboo.newStack.pop()
+        }
+        else
+        {
+            kaboo.currentPlayer?.hand = kaboo.usedStack.peek()
+            kaboo.usedStack.pop()
+        }
+
+    }
+    fun endTurn(){
+        val kaboo = rootService.currentGame
+        if (kaboo == null){
+            throw IllegalStateException("Game not started yet")
+        }
+
+        if (kaboo.currentPlayer == player1)
+            kaboo.currentPlayer = player2
+        else
+            kaboo.currentPlayer = player1
+    }
+
+    fun endGame(): String{
+        val kaboo = rootService.currentGame
+        if (kaboo == null){
+            throw IllegalStateException("Game not started yet")
+        }
+        var sum1 = 0
+        var sum2 = 0
+        for (i in 0..3){
+            sum1 += player1.deck[i].value.toInt()
+            sum2 += player2.deck[i].value.toInt()
+        }
+        //end game menu
+        if (sum1 > sum2)
+            return player1.name
+        else if (sum2 > sum1)
+            return player2.name
+        else
+            return "Draw"
+
     }
 }
